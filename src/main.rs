a9c42e13bb5f0f1733f38d72e542868b73c71c66
es183923 <72744903+es183923@users.noreply.github.com>
2022-10-26 17:39:39 -0400

    add clap and custom displayer

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,59 @@
+use clap::Parser;
 use human_repr::HumanDuration;
 use human_repr::HumanDurationData;
 use std::fmt;
 use windows::Media::Control::GlobalSystemMediaTransportControlsSessionManager;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// format of currently playing audio
+    #[arg(short, long, default_value = "%t - %a")]
+    format: String,
+}
+
 struct MediaInfo {
     pub title: String,
     pub artist: String,
-    pub time: HumanDurationData,
+    pub position: HumanDurationData,
 }
 
 impl fmt::Display for MediaInfo {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        if self.time != "0ns" && self.artist != "" {
-            write!(f, "{} - {} ({})", self.title, self.artist, self.time)
-        } else if self.artist != "" && self.time == "0ns" {
+        if self.position != "0ns" && !self.artist.is_empty() { // normal format
+            write!(f, "{} - {} ({})", self.title, self.artist, self.position)
+        } else if !self.artist.is_empty() && self.position == "0ns" { // position empty
             write!(f, "{} - {}", self.title, self.artist)
-        } else if self.time != "0ns" && self.artist == "" {
-            write!(f, "{} ({})", self.title, self.time)
+        } else if self.position != "0ns" && self.artist.is_empty() { // artist empty
+            write!(f, "{} ({})", self.title, self.position)
         } else {
             write!(f, "{}", self.title)
         }
     }
 }
 
+impl MediaInfo {
+    fn display(&self, f: String) -> String {
+        f
+            .replace("%t", &self.title)
+            .replace("%a", &self.artist)
+            .replace("%p", &format!("{}", &self.position))
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ()> {
+    let args = Args::parse();
+
     let playing = match get_media_info().await {
         Ok(song) => song,
         Err(_) => MediaInfo {
             title: "No Song Playing".to_owned(),
             artist: "".to_owned(),
-            time: 0.human_duration(),
+            position: 0.human_duration(),
         }, // No media playing
     };
-    println!("{}", playing);
+    println!("{}", playing.display(args.format));
     Ok(())
 }
 
@@ -72,7 +92,7 @@ async fn get_media_info() -> Result<MediaInfo, windows::core::Error> {
         Ok(stuff) => stuff,
         Err(err) => return Err(err),
     };
-    let time = match timeline.Position() {
+    let position = match timeline.Position() {
         Ok(stuff) => stuff,
         Err(err) => return Err(err),
     };
@@ -81,6 +101,6 @@ async fn get_media_info() -> Result<MediaInfo, windows::core::Error> {
     Ok(MediaInfo {
         title: title.to_string(),
         artist: artist.to_string(),
-        time: ((time.Duration / base.pow(7)).human_duration()),
+        position: ((position.Duration / base.pow(7)).human_duration()),
     })
 }
